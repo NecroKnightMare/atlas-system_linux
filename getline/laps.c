70ee1bcd9a4fad98aa1d78e0a6865c5d8ca5a3b3
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-01-13 17:30:54 +0000

    heaader has first prototype and c file is initiated

---
+++ getline/laps.c
@@ -0,0 +1,11 @@
+#include <laps.h>
+#include <stdio.h>
+
+void race_state(int *id, size_t size)
+{
+	int id = arr[i]
+	size = sizeofarr[]
+	
+	printf("Car %d joined the race.\n", x);
+	return race_state;
+}
---
+++ getline/laps.h
@@ -0,0 +1,6 @@
+#ifndef LAPS_H_
+#define LAPS_H_
+
+void race_state(int *id, size_t size);
+
+#endif
