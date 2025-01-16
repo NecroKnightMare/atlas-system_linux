7bf492caa0a3e49c1a98149591d91a4361a12a59
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-01-16 00:00:40 +0000

    Header file with struct for c file

--- getline/laps.h
+++ getline/laps.h
@@ -1,6 +1,15 @@
 #ifndef LAPS_H_
 #define LAPS_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Car {
+    int id;
+    int laps;
+    struct Car *next;
+} Car;
+
 void race_state(int *id, size_t size);
 
 #endif
