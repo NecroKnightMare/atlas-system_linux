c61ab81b89ce1febe834df741fd7f11fd44c57e7
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-01-17 19:53:17 +0000

    modified to pass filename into argv and updated libraries

--- ls/hls.c
+++ ls/hls.c
@@ -1,9 +1,10 @@
 #include <dirent.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
 /**
-* main- lists contents in hls dir
+* main- pass filename arguments to program
 * 
 * ls program to list contents of curr
 * dir without listing hidden files
@@ -11,25 +12,54 @@
 * Return: 0 if opened, 1 if not
 **/
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    DIR *dir;
-    struct dirent *entry;
-    /* Open the current directory */
-    dir = opendir(".");
-    if (dir == NULL) {
-        perror("opendir");
-        exit(1);
-    }
-    /* Read directory entries  added cond for hidden files to not show*/
-    while ((entry = readdir(dir)) != NULL)
-    {
-        if (entry->d_name[0] != '.') {
-        printf("%s  ", entry->d_name);
+    if (argc == 1) {
+        char *path = '.';
+        // DIR *dir;
+        /* Open the current directory */
+        dir = opendir(".");
+        if (dir == NULL) {
+            perror("opendir");
+            exit(1);
         }
+        /* Read directory entries  added cond for hidden files to not show*/
+        struct dirent *entry;
+        while ((entry = readdir(dir)) != NULL)
+        {
+            if (entry->d_name[0] != '.') {
+            printf("%s  ", entry->d_name);
+            }
+        }
+        printf("\n");
+        /* Close the directory */
+        closedir(dir);
+        return (0);
+        } else {
+        fprintf(stderr, "%s: %s\n", argv[0], stderror(errno));
+        return 1;
+        } else {
+            for (int i = 1; i < argc; i++) {
+                dir = opendir(path);
+                if (dir == NULL) {
+                    perror("opendir");
+                    exit(1);
+                }
+                /* Read directory entries  added cond for hidden files to not show*/
+                struct dirent *entry;
+                while ((entry = readdir(dir)) != NULL)
+                {
+                    if (entry->d_name[0] != '.') {
+                        printf("%s  ", entry->d_name);
+                    }
+                }
+                printf("\n");
+                /* Close the directory */
+                closedir(dir);
+                return (0);
+                } else {
+                    fprintf(stderr, "%s: %s\n", argv[0], stderror(errno));
+                    return 1;
+                }
+            }
     }
-    printf("\n");
-    /* Close the directory */
-    closedir(dir);
-    return (0);
-}
