026a041a5f9ee60cf0b597c4340ae69577403b19
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-01-17 19:03:06 +0000

    condition for hidden files

--- ls/hls.c
+++ ls/hls.c
@@ -6,16 +6,17 @@ int main(void)
     DIR *dir;
     struct dirent *entry;
     /* Open the current directory */
-    dir = opendir("");
-    if (dir == NULL)
-    {
+    dir = opendir(".");
+    if (dir == NULL) {
         perror("opendir");
         exit(1);
     }
-    /* Read directory entries */
+    /* Read directory entries  added cond for hidden files to not show*/
     while ((entry = readdir(dir)) != NULL)
     {
+        if (entry->d_name[0] != '.') {
         printf("%s  ", entry->d_name);
+        }
     }
     printf("\n");
     /* Close the directory */
