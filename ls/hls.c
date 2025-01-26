d191a857c93900dbcccbe321fe24c530525e3cb9
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-01-26 02:55:19 +0000

    created new file so function is not declared more than once

--- ls/hls.c
+++ ls/hls.c
@@ -9,22 +9,6 @@ void print_err(const char *program, const char *path, const char *error_mess) {
     fprintf(stderr, "%s: cannot access '%s': %s\n", program, path, error_mess);
 }
 
-void if_path(const char *path, const char *program) {
-    struct stat sb;
-    if (lstat(path, &sb) == 0) {
-        if (S_ISDIR(sb.st_mode)) {
-            printf("%s\n", path);
-            print_directory_contents(path);
-        } else if (S_ISREG(sb.st_mode)) {
-            printf("%s\n", path);
-        } else {
-            print_err(program, path, "Not a regular file or dir");
-        }
-    } else {
-        print_err(program, path, strerror(errno));
-    }
-}
-
 int main(int argc, const char *argv[]) {
     if (argc == 1) {
         print_directory_contents(".");
---
+++ ls/if_path.c
@@ -0,0 +1,15 @@
+void if_path(const char *path, const char *program) {
+    struct stat sb;
+    if (lstat(path, &sb) == 0) {
+        if (S_ISDIR(sb.st_mode)) {
+            printf("%s\n", path);
+            print_directory_contents(path);
+        } else if (S_ISREG(sb.st_mode)) {
+            printf("%s\n", path);
+        } else {
+            print_err(program, path, "Not a regular file or dir");
+        }
+    } else {
+        print_err(program, path, strerror(errno));
+    }
+}
