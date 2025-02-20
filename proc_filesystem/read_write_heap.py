397c37c6b053a9ccf9c7492aa533f6cc067ee4a7
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-02-20 02:20:51 +0000

    changed from gdb since it is incompatible with intranet checker, this uses c libraries to use ptrace to thread pid with memory and mapping

--- proc_filesystem/read_write_heap.py
+++ proc_filesystem/read_write_heap.py
@@ -1,70 +1,121 @@
-#!/usr/bin/python3
+#!/bin/usr/python3
 
+python
 """
-replace_string_in_heap.py: A Python script to find and replace a string in the heap memory of a running process using GDB.
+replace_string_in_heap.py: A Python script to find and replace a string in the heap memory of a running process using ptrace and ctypes.
 
 Usage:
-    python replace_string_in_heap.py <pid> <old_string> <new_string>
+    python replace_string_in_heap.py <pid>
 
 Arguments:
     <pid>          The process ID of the target process.
-    <old_string>   The string to find in the heap memory of the target process.
-    <new_string>   The string to replace the old string with in the heap memory of the target process.
 
 Example:
-    python replace_string_in_heap.py 1234 "old_string" "new_string"
+    python replace_string_in_heap.py 1234
+
+Modules:
+    os             The os module provides a way of using operating system-dependent functionality.
+    sys            The sys module provides access to some variables used or maintained by the interpreter.
+    ctypes         The ctypes module provides C compatible data types and allows calling functions in DLLs or shared libraries.
+
+Classes:
+    user_regs_struct: A ctypes Structure that represents the user registers of a process.
 """
-    
-import gdb
-import re
+
+import os
 import sys
+import ctypes
+import struct
 
-class ReplaceStringInHeap(gdb.Command):
-    def __init__(self):
-        super(ReplaceStringInHeap, self).__init__("replace_string", gdb.COMMAND_USER)
+# ptrace will send a specific tracee using one of these
+PTRACE_ATTACH = 16
+PTRACE_DETACH = 17
+PTRACE_PEEKDATA = 2
+PTRACE_POKEDATA = 5
 
-        def invoke(self, arg, from_tty):
-            args = arg.split()
-            if len(args) != 3:
-                print("Usage: replace_string <pid> <old_string> <new_string>")
-                return
-            
-            pid = args[0]
-            old_string = args[1]
-            new_string = args[2]
+class user_regs_struct(ctypes.Structure):
+    _fields_ = [("r15", ctypes.c_ulong),
+                ("r14", ctypes.c_ulong),
+                ("r13", ctypes.c_ulong),
+                ("r12", ctypes.c_ulong),
+                ("rbp", ctypes.c_ulong),
+                ("rbx", ctypes.c_ulong),
+                ("r11", ctypes.c_ulong),
+                ("r10", ctypes.c_ulong),
+                ("r9", ctypes.c_ulong),
+                ("r8", ctypes.c_ulong),
+                ("rax", ctypes.c_ulong),
+                ("rcx", ctypes.c_ulong),
+                ("rdx", ctypes.c_ulong),
+                ("rsi", ctypes.c_ulong),
+                ("rdi", ctypes.c_ulong),
+                ("orig_rax", ctypes.c_ulong),
+                ("rip", ctypes.c_ulong),
+                ("cs", ctypes.c_ulong),
+                ("eflags", ctypes.c_ulong),
+                ("rsp", ctypes.c_ulong),
+                ("ss", ctypes.c_ulong),
+                ("fs_base", ctypes.c_ulong),
+                ("gs_base", ctypes.c_ulong),
+                ("ds", ctypes.c_ulong),
+                ("es", ctypes.c_ulong),
+                ("fs", ctypes.c_ulong),
+                ("gs", ctypes.c_ulong)]
 
-            gdb.execute(f"attach {pid}")
+libc = ctypes.CDLL("libc.so.6")
 
-            output = gdb.execute("info proc mappings", to_string=True)
-            match = re.search(r"[heap\]s+([0-9a-f]+)", output)
-            if match:
-                start = int(match.group(1), 16)
-                end = int(match.group(2), 16)
-                heap_size = end - startprint(f"Heap found from {start:x} to {end:x}")
+# pid will thread ID of the corresponding Linux thread
+def attach(pid):
+    if libc.ptrace(PTRACE_ATTACH, pid, None, None) == -1:
+        raise OSError("ptrace attach failed")
+    os.waitpid(pid, 0)
 
-                heap_data = gdb.selected_inferior().read_memory(start, heap_size)
+def detach(pid):
+    if libc.ptrace(PTRACE_DETACH, pid, None, None) == -1:
+        raise OSError("ptrace detach failed")
 
-                offset = heap_data.find(old_string.encode())
-                if offset != -1:
-                    addr = start + offset
-                    print(f"Found '{old_string}' at {addr:x}")
+def peek_data(pid, addr):
+    word = libc.ptrace(PTRACE_PEEKDATA, pid, addr, None)
+    if word == -1:
+        raise OSError("ptrace peekdata failed")
+    return word
 
-                    gdb.selected_inferior().write_memory(addr, new_string.encode() + b'\x00')
-                    print(f"Replaced '{old_string}' with '{new_string}'")
-                else:
-                    print(f"'{old_string}' not found in heap")
+def poke_data(pid, addr, data):
+    if libc.ptrace(PTRACE_POKEDATA, pid, addr, data) == -1:
+        raise OSError("ptrace pokedata failed")
 
-                gdb.execute("detach")
+def find_and_replace_string(pid, old_str, new_str):
+    mem_path = f"/proc/{pid}/mem"
+    maps_path = f"/proc/{pid}/maps"
+    
+    with open(maps_path, "r") as maps_file:
+        for line in maps_file:
+            if "heap" in line:
+                addr_range = line.split()[0]
+                start, end = [int(x, 16) for x in addr_range.split("-")]
+                break
+    
+    with open(mem_path, "rb+") as mem_file:
+        mem_file.seek(start)
+        heap_data = mem_file.read(end - start)
+        
+        offset = heap_data.find(old_str.encode())
+        if offset == -1:
+            print("String not found in heap")
+            return
+        
+        mem_file.seek(start + offset)
+        mem_file.write(new_str.encode() + b'\x00' * (len(old_str) - len(new_str)))
 
-ReplaceStringInHeap()
-
-if __name__ == "main":
-    if len(sys.argv) != 4:
-        print(f"Usage: {sys.argv[0]} <pid> <old_string> <new_string>")
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <pid>")
         sys.exit(1)
-
-    pid = sys.argv[1]
-    old_string = sys.argv[2]
-    new_string = sys.argv[3]
-
-    gdb.execute(f"replace_string {pid} {old_string} {new_string}")
+    
+    pid = int(sys.argv[1])
+    
+    try:
+        attach(pid)
+        find_and_replace_string(pid, "old_string", "new_string")
+    finally:
+        detach(pid)
