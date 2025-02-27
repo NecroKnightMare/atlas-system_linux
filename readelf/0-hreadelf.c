e116de0314027a1827f97b7f421ce525fb7ceeaf
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-02-27 04:07:47 +0000

    reformatted- main up top and fixed braces

--- readelf/0-hreadelf.c
+++ readelf/0-hreadelf.c
@@ -4,6 +4,18 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+void print_elf_header(const char *filename);
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    print_elf_header(argv[1]);
+    return 0;
+}
+
 void print_elf_header(const char *filename) {
     int fd;
     Elf64_Ehdr header;
@@ -14,20 +26,19 @@ void print_elf_header(const char *filename) {
         exit(EXIT_FAILURE);
     }
 
-}
-
     if (read(fd, &header, sizeof(header)) != sizeof(header)) {
         perror("read");
         close(fd);
         exit(EXIT_FAILURE);
     }
 
+    close(fd);
+
     if (header.e_ident[EI_MAG0] != ELFMAG0 ||
         header.e_ident[EI_MAG1] != ELFMAG1 ||
         header.e_ident[EI_MAG2] != ELFMAG2 ||
         header.e_ident[EI_MAG3] != ELFMAG3) {
-        fprintf(stderr, "Not an ELF file.\n");
-        close(fd);
+        fprintf(stderr, "Not an ELF file\n");
         exit(EXIT_FAILURE);
     }
 
@@ -37,16 +48,21 @@ void print_elf_header(const char *filename) {
         printf("%02x ", header.e_ident[i]);
     }
     printf("\n");
+
     printf("  Class:                             %s\n",
-           header.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
+           header.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" :
+           header.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "Invalid class");
+
     printf("  Data:                              %s\n",
-           header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
+           header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" :
+           header.e_ident[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" : "Invalid data encoding");
+
     printf("  Version:                           %d (current)\n", header.e_ident[EI_VERSION]);
     printf("  OS/ABI:                            %d\n", header.e_ident[EI_OSABI]);
     printf("  ABI Version:                       %d\n", header.e_ident[EI_ABIVERSION]);
     printf("  Type:                              %d\n", header.e_type);
     printf("  Machine:                           %d\n", header.e_machine);
-    printf("  Version:                           0x%x\n", header.e_version);
+    printf("  Version:                           %d\n", header.e_version);
     printf("  Entry point address:               0x%lx\n", header.e_entry);
     printf("  Start of program headers:          %ld (bytes into file)\n", header.e_phoff);
     printf("  Start of section headers:          %ld (bytes into file)\n", header.e_shoff);
@@ -55,31 +71,6 @@ void print_elf_header(const char *filename) {
     printf("  Size of program headers:           %d (bytes)\n", header.e_phentsize);
     printf("  Number of program headers:         %d\n", header.e_phnum);
     printf("  Size of section headers:           %d (bytes)\n", header.e_shentsize);
-    printf("  Number of section headers:         %d\n", header->e_shnum);
-    printf("  Section header string table index: %d\n", header->e_shstrndx);
-}|
-
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-        print_error("Usage: ./0-hreadelf elf_filename");
-
-    int fd = open(argv[1], O_RDONLY);
-    if (fd == -1)
-        print_error("Error: Can't open file");
-
-    Elf64_Ehdr header;
-    if (read(fd, &header, sizeof(header)) != sizeof(header))
-        print_error("Error: Can't read file");
-
-    if (header.e_ident[EI_MAG0] != ELFMAG0 || 
-        header.e_ident[EI_MAG1] != ELFMAG1 || 
-        header.e_ident[EI_MAG2] != ELFMAG2 || 
-        header.e_ident[EI_MAG3] != ELFMAG3)
-        print_error("Error: Not an ELF file");
-
-    print_elf_header(&header);
-
-    close(fd);
-    return 0;
+    printf("  Number of section headers:         %d\n", header.e_shnum);
+    printf("  Section header string table index: %d\n", header.e_shstrndx);
 }
