8bc62c4803d4960f9327389ed193192b8a59e222
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-03-04 18:36:38 +0000

    specified with key byte and moved value from mem and into reg if different

--- libasm/1-strcmp.asm
+++ libasm/1-strcmp.asm
@@ -4,15 +4,17 @@ global asm_strcmp
     asm_strcmp:
         xor rax, rax                ; set RAX to 0
         .next_character:
-            mov al, [rdi]          ;Load byte from first string
-            cmp al, [rsi]           ;Compare to second string
+            mov al, byte [rdi]      ;Load byte from first string
+            cmp al, byte [rsi]      ;Compare byte to second string
             jne .diff               ;IF NOT ==, jump to diff
             test al, al             ;Check if \0
             je .done                ;IF \0 strings are ==
             inc rdi                 ;advance pointers
             inc rsi
-            jmp .next_character     ;increment
+            jmp .next_character     ;increment/loop
         .diff:
+            mov bl, byte [rsi]      ;Load value from memory into reg
             sub rax, [rsi]          ;return difference
+            mov rax, rax            ;Move result to rax
         .done:
         ret
