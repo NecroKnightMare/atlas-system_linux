0911a852d43f2035843d698a07092c9aadbf7c54
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-03-08 13:39:50 -0600

    added more steps to strcmp file and completed merges

--- libasm/1-strcmp.asm
+++ libasm/1-strcmp.asm
@@ -1,26 +1,29 @@
 BITS 64                             ;For RSI and RDI- they hold 64 bits
 
 section .text
-global asm_strcmp
+    global asm_strcmp
 
-    asm_strcmp:
-         xor rax, rax                ; set RAX to 0
-        .next_character:
+asm_strcmp:
+    xor rax, rax                ; set RAX to 0
 
-            mov rax [rdi]
-            movzx r x [rsi]
-            ;mov al, [rdi]      ;Load byte from first string
-            ;mov bl, [rsi]       ;Load byte from second string
-            cmp al, bl      ;Compare byte to second string
-            jne .diff               ;IF NOT ==, jump to diff
-            test al, al             ;Check if \0
-            je .done                ;IF \0 strings are ==
-            inc rdi                 ;advance pointers
-            inc rsi
-            jmp .next_character     ;increment/loop
-        .diff:
-            mov bl, byte [rsi]      ;Load value from memory into reg
-            ;sub al, bl              ;return difference of AL-BL
-            movzx rax, al           ;sign extend AL to RAX-mimics strcmp behavior
-        .done:
-            ret
+.next_character:
+    mov al, byte [rdi]      ;Load byte from first string
+    mov bl, byte [rsi]      ;Load byte to second string
+    cmp al, 0
+    jne .diff               ;IF NOT ==, jump to diff
+    cmp bl, 0
+    jne .diff
+    cmp al, bl
+    test al, al             ;Check if \0
+    je .done                ;IF \0 strings are ==
+    inc rdi                 ;advance pointers
+    inc rsi
+    jmp .next_character     ;increment/loop
+
+.diff:
+    mov bl, byte [rsi]      ;Load value from memory into reg
+    sub al, bl              ;return difference of AL-BL
+    movsx rax, al           ;sign extend AL to RAX-mimics strcmp behavior
+
+.done:
+    ret
