89609c4467a7c28284b6fdb997e180f19e8bf938
Ariel Lopez <alexandrea.lopez@atlasschool.com>
2025-03-08 19:01:57 +0000

    added extern values in main

--- libasm/3-main.c
+++ libasm/3-main.c
@@ -10,6 +10,11 @@
 #define C2  'S'
 #define C3  's'
 
+extern size_t asm_strlen(const char *str);
+extern int asm_strcmp(const char *s1, const char *s2);
+extern int asm_strncmp(const char *s1, const char *s2, size_t n);
+extern char *asm_strchr(const char *s, int c);
+
 /**
  * main - Program entry point
  *
