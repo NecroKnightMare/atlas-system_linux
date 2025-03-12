12fe58117cc82a9bffcb931f438b68b0cdd0c64d
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-03-12 10:40:45 -0500

    basically redid a lot, add ret to equal so doesn't jump down to diffferencae automatically

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -3,48 +3,35 @@ BITS 64                             ;For RSI and RDI- they hold 64 bits
 section .text
     global asm_strncmp
 
-;failing at s1, s3, 15
 
 asm_strncmp:
-    push rbp                ;base pointer - 8 bit
-    mov rbp, rsp            ;move stack* to base* - 8 bit
-    ;xor rax, rax
-    mov rdi, [rbp + 16]     ;address of s1 - 8 bit
-    mov rsi, [rbp + 24]     ;address of s2 - 8 bit
-    mov rcx, [rbp + 32]     ;counter of n or size_t - 8 bit
-
-    test rcx, rcx           ;if rcx == rcx
-    je .equal               ;jump to condition
+    xor rcx, rcx            ;counter register
 
 .next_character:
-    mov al, byte [rdi]      ;Load byte from first string
-    mov bl, byte [rsi]      ;Load byte to second string
+    cmp rcx, rdx           ;if rcx == rcx
+    jge .equal               ;jump to condition
+
+    mov al, byte [rdi + rcx]      ;Load byte from first string
+    mov bl, byte [rsi + rcx]      ;Load byte to second string
     
     cmp al, bl
     jne .difference
+
+
     test al, al             ;Check if \0
     jz .equal                ;IF \0 strings are ==
 
-    dec rcx                 ;decrement n
-    jz .equal               ;when n reaches 0
-
-    inc rdi                 ;advance pointers
-    inc rsi
+    inc rcx                 ;increment n
     jmp .next_character     ;increment/loop
 
 .equal:
     xor eax, eax          ; set to 0/return use 32 bit for efficiency
-
-.difference:
-    sub al, bl              ;difference of n
-    movzx eax, al           ;expand with zeros
-    jmp .function_end
-
-.function_end:
-    mov rax, 0
-    pop rbp
     ret
 
-;add size_t logic
-;rcx for 64bit counter
-;decrement with rcx using dec rcx before cmp
+.difference:
+    movzx eax, al
+    movzx ebx, bl
+    sub eax, ebx
+    ret
+
+;All good!
--- libasm/6-putc.asm
+++ libasm/6-putc.asm
@@ -3,13 +3,15 @@ BITS 64
 global asm_putc
 
 asm_putc:
+
     mov rax, 1             ; Syscall number for write (1)
     mov rdi, 1             ; File descriptor for stdout (1)
-    lea rsi, [rsp-1]           ; rsi = pointer to buffer on the stack
-    mov byte [rsp-1], dil         ; Store the character on the stack
+    lea rsi, [rsp-8]           ; rsi = pointer to buffer on the stack
+    mov byte [rsp-8], dil         ; Store the character on the stack
     mov rdx, 1             ; Number of bytes to write
 
     syscall
+
 
     mov rax, 1             ; Set rax to 1
     ret
