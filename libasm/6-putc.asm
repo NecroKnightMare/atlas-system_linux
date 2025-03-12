89c2bcc40ef3123245015169a3690ee492e59f29
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-03-12 00:26:02 -0500

    rem ush and pop

--- libasm/6-putc.asm
+++ libasm/6-putc.asm
@@ -3,8 +3,6 @@ BITS 64
 global asm_putc
 
 asm_putc:
-    push rdi               ;save data index
-
     mov rax, 1             ; Syscall number for write (1)
     mov rdi, 1             ; File descriptor for stdout (1)
     lea rsi, [rsp-1]           ; rsi = pointer to buffer on the stack
@@ -12,8 +10,6 @@ asm_putc:
     mov rdx, 1             ; Number of bytes to write
 
     syscall
-
-    pop rdi                 ;restore data index
 
     mov rax, 1             ; Set rax to 1
     ret
