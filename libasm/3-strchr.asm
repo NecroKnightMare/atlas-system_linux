09ba01c227df682a06c3a293f54b477d469f4879
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-03-11 22:44:12 -0500

    had to change al to sil line 14

--- libasm/3-strchr.asm
+++ libasm/3-strchr.asm
@@ -1,39 +1,25 @@
-section .data
+BITS 64
+
 section .text
-global asm_strchr
+    global asm_strchr
 
 asm_strchr:
-    ; Arguments:
-    ; rdi -> const char *s
-    ; esi -> int c
-
-    ; Save the registers we will use
-    push rdi
-    push rsi
-
-    ; Load the character to search for into al
-    mov al, byte [rsi]
+    xor rax, rax
     
-    ; Iterate over the string
-.loop:
+.next_character:
     movzx rdx, byte [rdi]    ; Load byte from string into rdx and zero-extend
     test dl, dl              ; Test if the byte is null terminator
     je .not_found            ; If null terminator, jump to not found
 
-    cmp dl, al               ; Compare the byte with the target character
+    cmp dl, sil               ; Compare the byte with the target character
     je .found                ; If match, jump to found
 
     inc rdi                  ; Move to the next byte in the string
-    jmp .loop                ; Repeat the loop
+    jmp .next_character      ; Repeat the loop
 
 .not_found:
     xor rax, rax             ; Clear rax to return NULL (0)
-    jmp .exit
+    ret
 
 .found:
     mov rax, rdi             ; Set rax to the address of the found character
-
-.exit:
-    pop rsi                 ;take rsi off the stack
-    pop rdi                 ;take rdi off of the stack
-    ret
