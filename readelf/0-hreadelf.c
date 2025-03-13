dea64efb2b495e732980d3169ca210385de8ce7f
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-03-12 22:53:26 -0500

    got it to compile will push to see what else I can do

--- readelf/0-hreadelf.c
+++ readelf/0-hreadelf.c
@@ -5,60 +5,63 @@
 #include <unistd.h>
 #include <elf.h>
 
-void print_error(const char *msg)
-{
-    fprintf(stderr, "%s\n", msg);
-    exit(98);
-}
+void print_elf_header(const char *filename);
 
-// const char *get_type(uint16_t e_type)
-// {
-//     switch (e_type)
-//     {
-//         case ET_NONE: return "NONE (No file type)";
-//         case ET_REL:  return "REL (Relocatable file)";
-//         case ET_EXEC: return "EXEC (Executable file)";
-//         case ET_DYN:  return "DYN (Shared object file)";
-//         case ET_CORE: return "CORE (Core file)";
-//         default:      return "Unknown";
-//     }
-// }
-
-void read_elf_header(const char *filename)
-{
-    int fd;
-    Elf64_Ehdr header;
-
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        print_error("Error: Cannot open file");
-
-    if (read(fd, &header, sizeof(header)) != sizeof(header))
-    {
-        close(fd);
-        print_error("Error: Cannot read ELF header");
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
+    print_elf_header(argv[1]);
+    return 0;
+}
+
+void print_elf_header(const char *filename) {
+    int fd;
+    Elf32_Ehdr header;
+
+    fd = open(filename, O_RDONLY);
+    if (fd < 0) {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+
+    if (read(fd, &header, sizeof(header)) != sizeof(header)) {
+        perror("read");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+    
+    // using to debug// can't figure out to test, says I don't have
+    // sortix32 in my directory? Don't even know how to go about that.
+    printf("EI_OSABI: %x\n", header.e_ident[EI_OSABI]); 
+    
+    close(fd);
+
+    // Validate ELF magic
     if (header.e_ident[EI_MAG0] != ELFMAG0 ||
         header.e_ident[EI_MAG1] != ELFMAG1 ||
         header.e_ident[EI_MAG2] != ELFMAG2 ||
-        header.e_ident[EI_MAG3] != ELFMAG3)
-    {
-        close(fd);
-        print_error("Error: Not an ELF file");
+        header.e_ident[EI_MAG3] != ELFMAG3) {
+        fprintf(stderr, "Not an ELF file\n");
+        exit(EXIT_FAILURE);
     }
 
     printf("ELF Header:\n");
     printf("  Magic:   ");
-    for (int i = 0; i < EI_NIDENT; i++)
+    for (int i = 0; i < EI_NIDENT; i++) {
         printf("%02x ", header.e_ident[i]);
+    }
     printf("\n");
 
     printf("  Class:                             %s\n",
-           header.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
+           header.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" :
+           header.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "Invalid class");
 
     printf("  Data:                              %s\n",
-           header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
+           header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" :
+           header.e_ident[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" : "Invalid data encoding");
 
     printf("  Version:                           %d (current)\n", header.e_ident[EI_VERSION]);
 
@@ -101,29 +104,14 @@ void read_elf_header(const char *filename)
     }
 
     printf("  Version:                           0x%x\n", header.e_version);
-    printf("  Entry point address:               0x%lx\n", header.e_entry);
-    printf("  Start of program headers:          %lu (bytes into file)\n", header.e_phoff);
-    printf("  Start of section headers:          %lu (bytes into file)\n", header.e_shoff);
+    printf("  Entry point address:               0x%lx\n", (unsigned long)header.e_entry);
+    printf("  Start of program headers:          %d (bytes into file)\n", header.e_phoff);
+    printf("  Start of section headers:          %d (bytes into file)\n", header.e_shoff);
     printf("  Flags:                             0x%x\n", header.e_flags);
-    printf("  Size of this header:               %u (bytes)\n", header.e_ehsize);
-    printf("  Size of program headers:           %u (bytes)\n", header.e_phentsize);
-    printf("  Number of program headers:         %u\n", header.e_phnum);
-    printf("  Size of section headers:           %u (bytes)\n", header.e_shentsize);
-    printf("  Number of section headers:         %u\n", header.e_shnum);
-    printf("  Section header string table index: %u\n", header.e_shstrndx);
-
-    close(fd);
-}
-
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
-        exit(98);
-    }
-
-    read_elf_header(argv[1]);
-
-    return 0;
+    printf("  Size of this header:               %d (bytes)\n", header.e_ehsize);
+    printf("  Size of program headers:           %d (bytes)\n", header.e_phentsize);
+    printf("  Number of program headers:         %d\n", header.e_phnum);
+    printf("  Size of section headers:           %d (bytes)\n", header.e_shentsize);
+    printf("  Number of section headers:         %d\n", header.e_shnum);
+    printf("  Section header string table index: %d\n", header.e_shstrndx);
 }
--- readelf/hreadelf.h
+++ readelf/hreadelf.h
@@ -7,8 +7,6 @@
 #include <unistd.h>
 #include <elf.h>
 
-// const char *get_type(uint16_t e_type);
-void print_error(const char *msg);
-void read_elf_header(const char *filename);
+void print_elf_header(const char *filename);
 
 #endif 
