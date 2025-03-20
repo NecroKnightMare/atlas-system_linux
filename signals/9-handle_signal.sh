4fd1b3910edb2f0c8dbbcbf073ee78513b915cd2
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-03-19 22:01:02 -0500

    yup, rem comments

--- signals/9-handle_signal.sh
+++ signals/9-handle_signal.sh
@@ -1,9 +1,2 @@
-# Write a sh script that set a handler for the signals SIGABRT, SIGIO and SIGTERM
-
-# The script must print Nope followed by a new line, each time a SIGABRT, SIGIO or SIGTERM is caught
-# Your script must contain a shebang
-# You’re not allowed to have more than 2 lines in your script
-# You’re not allowed to have more than 1 instruction per line
-
-# trap/catch signals
+#!/bin/bash
 for s in ABRT IO TERM; do trap 'echo Nope' SIG$s; done
