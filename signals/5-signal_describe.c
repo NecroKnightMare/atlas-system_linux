e374cba1f0f2d6b6090a671029426cd5ae06fd8b
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-03-19 19:18:36 -0500

    used arg logic and atoi to get right output integer

--- signals/5-signal_describe.c
+++ signals/5-signal_describe.c
@@ -1,4 +1,7 @@
 #include "signals.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /*Write a program that prints a description of a given signal
 
@@ -9,4 +12,25 @@ You’re not allowed to have more than 1 function in your file
 You’re not allowed to have more than 12 lines in your function
 You can assume that if a parameter is given, it will be a number
 */
-./describe <signum>
+int main (int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: %s <signum>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    /*convert string to integer*/
+    int signum = atoi(argv[1]);
+    char *signal_name = strsignal(signum);
+
+    if (signal_name)
+    {
+    printf("%d: %s\n", signum, signal_name);
+    return (EXIT_SUCCESS);
+    } else {
+        printf("%d: Unknown signal\n", signum);
+        return (EXIT_FAILURE);
+    }
+    return EXIT_SUCCESS;
+}
