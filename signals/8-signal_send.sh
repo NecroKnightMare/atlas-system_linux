17fa6a7bfc953f9fa108f2c969cfeff69f73a104
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-03-19 21:58:16 -0500

    added main, had one line of code may need to erase comments for checker

--- signals/8-signal_send.sh
+++ signals/8-signal_send.sh
@@ -6,6 +6,8 @@
 # Where <pid> is the PID of the process to send a signal to
 # If the number of arguments is not correct, your program must print Usage: %s <pid> (where %s is argv[0]), followed by a new line, and exit with 1
 
+
+pid=$1
 # makes sure there's only one argument; exits otherwise
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <pid>"
@@ -13,16 +15,15 @@ if [ "$#" -ne 1 ]; then
 fi
 
 # saves the first argument as the pid
-pid=$1
+# pid=$1
 
 # sends the SIGQUIT signal to the process with the given pid
 # kill -3 "$pid"
 # checks if the last signal was sent successfully
-
+kill -SIGQUIT "$pid"
 if ! kill -SIGQUIT <PID>; then
     echo "Failed to send SIGQUIT to process $pid"
     exit 1
 #  saying permissions denied here in error
 
 fi
-echo "SIGQUIT signal sent to process $pid"
--- signals/9-handle_signal.sh
+++ signals/9-handle_signal.sh
@@ -5,3 +5,5 @@
 # You’re not allowed to have more than 2 lines in your script
 # You’re not allowed to have more than 1 instruction per line
 
+# trap/catch signals
+for s in ABRT IO TERM; do trap 'echo Nope' SIG$s; done
