977a2ffb12521f3bf92d79c29f0ddea88f9637d7
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-03-28 13:04:34 -0500

    compiles, will see what happens

--- 0x08_CPython/1-python.c
+++ 0x08_CPython/1-python.c
@@ -1,14 +1,36 @@
-#include <Python.h>
+#include <Python.h> // use gcc $(python-config --include) when compiling
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <float.h>
-#include "listobject.h"
-#include "object.h"
+#include <time.h>
+#include "list_object.h"
 
 
 
 void print_python_list(PyObject *p)
 {
-    print(p);
+    if (!PyList_Check(p)) {
+        fprintf(stderr, "Invalid PyObject: Not a list\n");
+        return;
+    }
+
+
+    Py_ssize_t size = PyList_Size(p);
+    printf("[*] Python list info\n");
+    for (Py_ssize_t i = 0; i < size; i++) 
+    {
+    PyObject *item = PyList_GetItem(p, i);
+        if (item == NULL) 
+        {
+            fprintf(stderr, "Error: Item %zd is NULL\n", i);
+            continue;
+        }
+        PyList_Append(p, item);
+        if (item == NULL)
+        {
+            fprintf(stderr, "Error: Item %zd is -1\n", i);
+            continue;
+        }
+    }
 }
--- 0x08_CPython/list_object.h
+++ 0x08_CPython/list_object.h
@@ -1,11 +1,13 @@
 #ifndef LIST_OBJECT_H
 #define LIST_OBJECT_H
 
+#include <Python.h>
+
 
 void print_python_list(PyObject *p);
-void print_python_bytes(PyObject *p);
-void print_python_float(PyObject *p);
-void print_python_string(PyObject *p);
-void print_python_int(PyObject *p);
+// void print_python_bytes(PyObject *p);
+// void print_python_float(PyObject *p);
+// void print_python_string(PyObject *p);
+// void print_python_int(PyObject *p);
 
 #endif
