d0cbb9258e84b9e38639415d07089aef4f59420f
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-03-28 13:37:21 -0500

    still can't get Element to print correct

--- 0x08_CPython/1-python.c
+++ 0x08_CPython/1-python.c
@@ -10,46 +10,54 @@
 
 void print_python_list(PyObject *p)
 {
-    if (!PyList_Check(p)) {
+    if (!PyList_Check(p)) 
+    {
         fprintf(stderr, "Invalid PyObject: Not a list\n");
         return;
     }
 
     Py_ssize_t size = PyList_Size(p);
+    // Py_ssize_t allocated = ((PyListObject *)p)->allocated;
+
+    // printf("[*] Python list info\n");
+    // printf("[*] Size of the Python List = %zd\n", size);
+    // printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
     
     for (Py_ssize_t i = 0; i < size; i++) 
     {
-    PyObject *item = PyList_GetItem(p, i);
-        if (item == NULL) 
+        PyObject *item = PyList_GetItem(p, i);
+        if (PyUnicode_Check(item))
         {
-            fprintf(stderr, "Error: Item %zd is NULL\n", i);
-            continue;
-        }
-        if (item == Py_None) 
+            printf("str\n");
+        } else if (PyLong_Check(item))
         {
-            fprintf(stderr, "Error: Item %zd is None\n", i);
-            continue;
-        }
-        if (item == Py_False) 
+            printf("int\n");
+        } else if (PyFloat_Check(item))
         {
-            fprintf(stderr, "Error: Item %zd is False\n", i);
-            continue;
+            printf("float\n");
+        } else if (item == Py_None)
+        {
+            printf("NoneType\n");
+        } else {
+            printf("unknown\n");
         }
-       
+        printf("Element %zd: ", i);
+        
         printf("[*] Python list info\n");
         printf("[*] Size of the Python List = %zd\n", size);
         printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
-        // output statement is wrong-should be:
-        // Element 0: str
-        // not : Element 0: [*] Python list info
-        // Not i or size, logic may be missing
-        printf("Element %zd: ", i);
+            
+            // output statement is wrong-should be:
+            // Element 0: str
+            // not : Element 0: [*] Python list info
+            // Not i or size, logic may be missing
+            // printf("Element %zd: ", i);
 
-        // PyList_Append(p, item);
-        // if (item == NULL)
-        // {
-        //     fprintf(stderr, "Error: Item %zd is -1\n", i);
-        //     continue;
-        // }
+            // PyList_Append(p, item);
+            // if (item == NULL)
+            // {
+            //     fprintf(stderr, "Error: Item %zd is -1\n", i);
+            //     continue;
+            // }
     }
 }
