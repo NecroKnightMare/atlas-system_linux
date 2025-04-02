6eec652087c507930d2201d6a2361d6cd65ee526
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-04-02 04:43:45 -0500

    compiles but won't run due to invisible syntax error

--- 0x08_CPython/2-python.c
+++ 0x08_CPython/2-python.c
@@ -1,11 +1,74 @@
-#include <Python.h> // use gcc $(python-config --include) when compiling
+#include <Python.h> // use gcc $(python-config --includes) when compiling
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <float.h>
 #include "list_object.h"
 
-// call function 
-void print_python_list(PyObject *p);
-
-void print_python_bytes(PyObject *p);
+void print_python_list(PyObject *p)
 {
+    if (!PyList_Check(p)) 
+    {
+        fprintf(stderr, "Invalid PyObject: Not a list\n");
+        return;
+    }
+
+    Py_ssize_t size = PyList_Size(p);
+    // Py_ssize_t allocated = ((PyListObject *)p)->allocated;
+
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python List = %zd\n", size);
+    printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
     
+    for (Py_ssize_t i = 0; i < size; i++) 
+    {
+        // Task 1 PyObject *item = PyList_GetItem(p, i);
+        // Task 2 line 29
+        PyObject *item = ((PyListObject *)p)->ob_item[i];
+        if (PyUnicode_Check(item))
+        {
+            printf("Element %zd: str\n", i);
+        } else if (PyLong_Check(item))
+        {
+            printf("Element %zd: int\n", i);
+        } else if (PyFloat_Check(item))
+        {
+            printf("Element %zd: float\n", i);
+        } else if (PyTuple_Check(item))
+        {
+            printf("Element %zd: tuple\n", i);
+        } else if (PyList_Check(item))
+        {
+            printf("Element %zd: list\n", i);
+        } else if (item == Py_None)
+        {
+            printf("Element %zd: NoneType\n", i);
+        } else {
+            printf("Element %zd: unknown\n", i);
+        }
+        
+    }
+}
+
+void print_python_bytes(PyObject *p)
+{
+   if (!PyBytes_Check(p)) 
+   {
+       fprintf(stderr, "Invalid PyObject_Bytes\n");
+       return;
+   }
+
+    Py_ssize_t size = PyBytes_Size(p);
+    char *bytes = PyBytes_AsString(p);
+
+    printf("[.] Python bytes info\n");
+    printf(" size: %zd bytes:" , size);
+    printf(": trying string: %s\n", bytes);
+
+    printf(" first %zd bytes:", size > 10 ? 10 : size);
+    for (Py_ssize_t i = 0; i < size && i < 10; i++) 
+    {
+        printf("%02x ", (unsigned char)bytes[i]);
+    }
+    printf("\n");
 }
