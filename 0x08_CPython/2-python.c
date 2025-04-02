24d1ffa2d24e02d9bf351fc83989dba37dc32b27
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-04-02 05:09:17 -0500

    syntax

--- 0x08_CPython/2-python.c
+++ 0x08_CPython/2-python.c
@@ -66,7 +66,7 @@ void print_python_bytes(PyObject *p)
     printf(" trying string: %s\n", bytes);
 
     Py_ssize_t print_size = (size > 10) ? 10 : size;
-    printf(" first %zd bytes: ", print_size + 1);
+    printf(" first %zd bytes:", print_size + 1);
     for (Py_ssize_t i = 0; i < size && i < 10; i++) 
     {
         printf(" %02x", (unsigned char)bytes[i]);
