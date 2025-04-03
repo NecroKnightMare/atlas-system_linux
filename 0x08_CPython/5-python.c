a72b04eea1d3644ceb0270952ca57c7cbcae97ec
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-04-02 22:53:33 -0500

    file 5 compiled, not tested yet

---
+++ 0x08_CPython/5-python.c
@@ -0,0 +1,38 @@
+#include <Python.h>
+#include <stdio.h>
+#include "list_object.h"
+
+void print_python_int(PyObject *p)
+{
+    if (!PyLong_Check(p))
+    {
+        fprintf(stderr, "[ERROR] Invalid Int Object\n");
+        return;
+    }
+
+    PyLongObject *long_obj = (PyLongObject *)p;
+    Py_ssize_t size = ((PyVarObject *)p)->ob_size;
+    unsigned long int result = 0;
+    
+    if (size < 0)
+    {
+        size = -size;
+    }
+
+    for (Py_ssize_t i = 0; i < size; i++)
+    {
+        if (i > (Py_ssize_t)(sizeof(unsigned long int) * 8 / PyLong_SHIFT))
+        {
+            printf("C unsigned long int overflow\n");
+            return;
+        }
+        result += long_obj->ob_digit[i] << (i * PyLong_SHIFT);
+    }
+
+    if (((PyVarObject *)p)->ob_size < 0)
+        printf("-%lu\n", result);
+    else
+        printf("%lu\n", result);
+
+    fflush(stdout);
+}
--- 0x08_CPython/list_object.h
+++ 0x08_CPython/list_object.h
@@ -8,6 +8,6 @@ void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
  void print_python_float(PyObject *p);
 void print_python_string(PyObject *p);
-// void print_python_int(PyObject *p);
+void print_python_int(PyObject *p);
 
 #endif
