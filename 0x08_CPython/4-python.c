672b9f0f3171f2afadde46a485329ee2ebedda90
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-04-03 08:53:28 -0500

    added current output in file 4

--- 0x08_CPython/4-python.c
+++ 0x08_CPython/4-python.c
@@ -56,3 +56,30 @@ void print_python_string(PyObject *p)
     printf("  value: %s\n", value);
     }
 }
+// Current output:
+// 0x08_CPython# python3 ./4-tests.py
+// [.] string object info
+//   type: compact ascii
+//   length: 24
+//   value: The spoon does not exist
+// [.] string object info
+//   type: compact unicode object
+//   length: 19
+//   value: ложка не существует
+// [.] string object info
+//   type: compact unicode object
+//   length: 24
+//   value: La cuillère n'existe pas
+// [.] string object info
+//   type: compact unicode object
+//   length: 5
+//   value: 勺子不存在
+// [.] string object info
+//   type: compact unicode object
+//   length: 14
+//   value: 숟가락은 존재하지 않는다.
+// [.] string object info
+//   type: compact unicode object
+//   length: 10
+//   value: スプーンは存在しない
+// [ERROR] Invalid String Object
