0e2da7493b4ce942b20eedbcf0ecfd6f69d7a4f8
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-04-03 09:27:28 -0500

    added spaces b4 error

--- 0x08_CPython/4-python.c
+++ 0x08_CPython/4-python.c
@@ -38,7 +38,7 @@ void print_python_string(PyObject *p)
 
     if (!PyUnicode_Check(p))
     {
-        printf("[ERROR] Invalid String Object\n");
+        printf("  [ERROR] Invalid String Object\n");
         // fflush(stdout);
         return;
     }
