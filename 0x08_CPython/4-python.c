4450bf31dbbae7b66c278d7033bd0a192a725d09
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-04-03 09:42:31 -0500

    removed old output and replaced. Task is 100% complete

--- 0x08_CPython/4-python.c
+++ 0x08_CPython/4-python.c
@@ -3,35 +3,11 @@
 #include <unicodeobject.h>
 #include "list_object.h"
 
-// Tried removing fflush and isolating print 
-// statements still get wrong output for checker
+// Correct output for the function print_python_list
 
-// Correct output - case: s = b"There is no spoon"
-
-// [copy_files] Filed copied: main_2.py
-// [copy_files] Filed copied: output_2
-// [exec_bash] Command to run:
-// cat output_2
-// su student_jail -c 'timeout 30 bash -c '"'"'cat output_2'"'"''
-// [exec_bash] Return code: 0
-// [exec_bash] Student stdout:
-// [.] string object info
-//   [ERROR] Invalid String Object
-// [exec_bash] Student stdout length: 55
-// [exec_bash] Student stderr:
-// [exec_bash] Student stderr length: 0
-// [exec_bash] Command to run:
-// LANG=C.UTF-8 timeout 10s ./main_2.py
-// su student_jail -c 'timeout 30 bash -c '"'"'LANG=C.UTF-8 timeout 10s ./main_2.py'"'"''
-// [exec_bash] Return code: 0
-// [exec_bash] Student stdout:
-// [ERROR] Invalid String Object
-// [exec_bash] Student stdout length: 30
-// [exec_bash] Student stderr:
-// [exec_bash] Student stderr length: 0
 void print_python_string(PyObject *p)
 {
-    // Somewhere in this logic its printing an extra line...
+
 
     printf("[.] string object info\n");
     fflush(stdout);
@@ -39,7 +15,6 @@ void print_python_string(PyObject *p)
     if (!PyUnicode_Check(p))
     {
         printf("  [ERROR] Invalid String Object\n");
-        // fflush(stdout);
         return;
     }
 
@@ -54,14 +29,12 @@ void print_python_string(PyObject *p)
     }
     else
     {
-    //printf("[.] string object info\n");
     printf("  type: %s\n", type);
     printf("  length: %zd\n", length);
     printf("  value: %s\n", value);
     }
 }
 // Current output:
-// 0x08_CPython# python3 ./4-tests.py
 // [.] string object info
 //   type: compact ascii
 //   length: 24
@@ -86,4 +59,18 @@ void print_python_string(PyObject *p)
 //   type: compact unicode object
 //   length: 10
 //   value: スプーンは存在しない
-// [ERROR] Invalid String Object
+// [.] string object info
+//   [ERROR] Invalid String Object
+//   length: 10
+//   value: スプーンは存在しない
+//   length: 10
+//   value: スプーンは存在しない
+// [.] string object info
+//   [ERROR] Invalid String Object
+//   length: 10
+//   value: スプーンは存在しない
+// [.] string object info
+//   length: 10
+//   value: スプーンは存在しない
+// [.] string object info
+//   [ERROR] Invalid String Object
