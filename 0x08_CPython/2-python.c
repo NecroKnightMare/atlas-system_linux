a4b1c7067afd107962127b59878976988903c690
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-04-02 21:47:57 -0500

    added new line back

--- 0x08_CPython/2-python.c
+++ 0x08_CPython/2-python.c
@@ -69,11 +69,11 @@ void print_python_bytes(PyObject *p)
     printf("  first %zd bytes: ", print_size + 1);
     for (Py_ssize_t i = 0; i < size && i < 10; i++) 
     {
-        printf("%02x ", (unsigned char)bytes[i]);
+        printf(" %02x ", (unsigned char)bytes[i]);
     }
     if (size < 10)
     {
         printf("%02x", 0);
     }
-    //printf("\n");
+    printf("\n");
 }
