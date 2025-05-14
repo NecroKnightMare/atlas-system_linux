1f478181e4b014e316fd8e73390bebb22a875c5b
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-05-14 00:08:35 -0500

    task 1, copied from 0 added string line

--- strace/strace_1.c
+++ strace/strace_1.c
@@ -3,19 +3,21 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/reg.h>
+#include <string.h>
+#include <errno.h>
 #include <stdlib.h>
-#include <sys/user.h>
+#include <sys/user.h> // For user_regs_struct
 #include "syscalls.h"
-// Change up code to print strings
 
-syscall_t ;
+
+int trace_child(int argc, char **argv);
+int trace(pid_t child);
 
 int main(int argc, char *argv[]) {
-    syscall_t const syscalls_64_g;
-
     if (argc < 2) {
         fprintf(stderr, "Usage: %s command [args...]\n", argv[0]);
-        return EXIT_FAILURE;
+        exit(1);
     }
 
     pid_t child = fork();
@@ -24,35 +26,76 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
     if (child == 0) {
-        // Child process: request tracing and execute command
-        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-        execv(argv[1], &argv[1]);
-        perror("execv failed");
-        return EXIT_FAILURE;
+        return trace_child(argc-1, argv+1);
     } else {
-        // Parent process: trace system calls
-        int status;
+        return trace(child);
+    }
+}
 
+int trace_child(int argc, char **argv) 
+{
+    char *args [argc+1];
+    memcpy(args, argv, argc * sizeof(char *));
+    args[argc] = NULL;
+
+    ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+    kill(getpid(), SIGSTOP);
+    return execvp(args[0], args);
+}
+
+int wait_syscall(pid_t child);
+
+int trace(pid_t child)
+{
+    int status, retval;
+    struct user_regs_struct regs;
+
+    waitpid(child, &status, 0);
+    ptrace(PTRACE_SETOPTIONS, child, NULL, PTRACE_O_TRACESYSGOOD);
+
+    while(1)
+    {
+        if (wait_syscall(child) != 0) break;
+
+        if (ptrace(PTRACE_GETREGS, child, NULL, &regs) == -1)
+        {
+            perror("ptrace GETREGS failed");
+            return EXIT_FAILURE;
+        }
+
+
+        // task 2
+        int syscall = ptrace(PTRACE_PEEKUSER, child, sizeof(long) * ORIG_RAX);
+        retval = ptrace(PTRACE_PEEKUSER, child, sizeof(long) * RAX);
+        fprintf(stderr, "syscall(%d) = ", syscall);
+        
+        if (wait_syscall(child) != 0) break;
+
+        //DEBUGGING 38
+        if (retval == -38) {
+            fprintf(stderr, "Warning: Invalid syscall return (-38). Possible GETREGS failure.\n");
+            fflush(stderr);
+        }
+        printf("syscall(%lld) = %lld\n", (long long)syscall, (long long)retval);
+        fflush(stdout);
+    }
+    return 0;
+}
+
+int wait_syscall(pid_t child)
+{
+    int status;
+    while(1)
+    {
+        ptrace(PTRACE_SYSCALL, child, 0, 0);
         waitpid(child, &status, 0);
-        // this ptrace was the infinite loop problem (dont put syscall here)
-        ptrace(PTRACE_SETOPTIONS, child, NULL, PTRACE_O_TRACESYSGOOD);
-
-        while (!WIFEXITED(status)) {
-            ptrace(PTRACE_SYSCALL, child, NULL, NULL);
-
-            if (WIFEXITED(status)) break;
-
-            if (WIFSTOPPED(status))
-            {
-                // if (ptrace(child, NULL, &syscall) == -1)
-                {
-                    perror("ptrace GETREGS failed");
-                    return EXIT_FAILURE;
-                }
-                // should be here that the only thing changes
-                printf("%c\n", syscall_t);
-            }
+        if (WIFSTOPPED(status) && WSTOPSIG(status) & 0x80)
+        {
+            return 0;
+        }
+        if (WIFEXITED(status))
+        {
+            return 1;
         }
     }
-    return EXIT_SUCCESS;
 }
