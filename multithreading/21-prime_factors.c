041952a21b5308454338057b3511c3b4a46db312
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-05-14 20:30:33 -0500

    took FOREVER cause I thought there was a grammatical error for prime function.... added it to header

---
+++ multithreading/21-prime_factors.c
@@ -0,0 +1,128 @@
+// #include <stdlib.h>
+// #include <stdio.h>
+// #include <string.h>
+// #include <math.h>
+// #include "list.h"
+
+
+// /**
+//  * prime_factors - Factorizes number into a list of prime factors
+//  * @s: String representation of the number to factorize
+//  * 
+//  * Return: List of prime factors, or NULL on failure
+//  */
+// list_t *prime_factors(char const *s)
+//  if (!s || !strlen(s))  // Ensure valid input
+//         return NULL;
+
+//     list_t *factors = list_init();
+//     if (!factors)
+//         return NULL;
+
+//     unsigned long n = strtoul(s, NULL, 10);
+//     unsigned long i;
+
+//     for (i = 2; i * i <= n; i++)  // Stop at sqrt(n)
+//     {
+//         while (n % i == 0)
+//         {
+//             unsigned long *factor = malloc(sizeof(unsigned long));
+//             if (!factor)
+//             {
+//                 free(factor);
+//                 list_destroy(factors, free);
+//                 return NULL;
+//             }
+//             *factor = i;
+//             list_add(factors, factor);
+//             n /= i;
+//         }
+//     // any remaining prime factor
+//     if (n > 1)
+//     {
+//         unsigned long *factor = malloc(sizeof(unsigned long));
+//         if (!factor)
+//         {
+//             free(factor);
+//             list_destroy(factors, free);
+//             return NULL;
+//         }
+//     *factor = n;
+//     list_add(factors, factor);
+//     }
+// return factors;
+// }
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "list.h"
+
+/**
+ * prime_factors - Factorizes number into a list of prime factors
+ * @s: String representation of the number to factorize
+ * 
+ * Return: List of prime factors, or NULL on failure
+ */
+list_t *prime_factors(char const *s)
+{
+    if (!s || !strlen(s))  // Ensure valid input
+        return NULL;
+
+    list_t *factors = list_init(NULL);
+    if (!factors)
+        return NULL;
+
+    unsigned long n = strtoul(s, NULL, 10);
+    if (n < 2)  // No need to factorize 0 or 1
+    {
+        free(factors);
+        return NULL;
+    }
+
+    // Handle factorization by 2 separately for efficiency
+    while (n % 2 == 0)
+    {
+        unsigned long *factor = malloc(sizeof(unsigned long));
+        if (!factor)
+        {
+            list_destroy(factors, free);
+            return NULL;
+        }
+        *factor = 2;
+        list_add(factors, factor);
+        n /= 2;
+    }
+
+    // Check odd factors up to sqrt(n)
+    for (unsigned long i = 3; i * i <= n; i += 2)
+    {
+        while (n % i == 0)
+        {
+            unsigned long *factor = malloc(sizeof(unsigned long));
+            if (!factor)
+            {
+                list_destroy(factors, free);
+                return NULL;
+            }
+            *factor = i;
+            list_add(factors, factor);
+            n /= i;
+        }
+    }
+
+    // If remaining n is prime, add it
+    if (n > 1)
+    {
+        unsigned long *factor = malloc(sizeof(unsigned long));
+        if (!factor)
+        {
+            list_destroy(factors, free);
+            return NULL;
+        }
+        *factor = n;
+        list_add(factors, factor);
+    }
+
+    return factors;
+}
