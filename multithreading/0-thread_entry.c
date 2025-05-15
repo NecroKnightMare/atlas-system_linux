d0d4d3c5f17084603a9aaa7b787f8f48b34e3fd4
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2025-05-14 21:17:22 -0500

    documentation

--- multithreading/0-thread_entry.c
+++ multithreading/0-thread_entry.c
@@ -2,6 +2,17 @@
 #include <pthread.h>
 #include "multithreading.h"
 
+/**
+ * thread_entry - Entry point for a new thread
+ * @arg: Pointer to thread argument (expected as a string)
+ *
+ * Description:
+ * This function prints the given string argument and then
+ * properly terminates the thread using `pthread_exit()`.
+ *
+ * Return: Always returns NULL
+ */
+
 void *thread_entry(void *arg)
 {
 	char *str = (char *)arg;
