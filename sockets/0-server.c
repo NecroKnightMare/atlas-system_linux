f1a8ebea3ab48275db43712373ef4b0f3b775030
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-06-25 12:02:15 -0500

    betty doc in file 0

--- sockets/0-server.c
+++ sockets/0-server.c
@@ -7,12 +7,21 @@
 
 #define PORT 12345
 
+/*
+ * main - Entry point of the server program
+ *
+ * Creates IPv4 TCP socket, binds it to port 12345,
+ * listens for incoming connections, and blocks
+ *
+ * Return: Always 0 success, exits on failure
+*/
+
 int main(void)
 {
 	int server_fd;
 	struct sockaddr_in address;
 
-    /* Create socket file descriptor (IPv4, TCP, default protocol)*/
+    /* Create socket file descriptor IPv4*/
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == -1)
     {
