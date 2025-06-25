75d73cefaca2ae86f5971f5faad10ed54e72666b
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-06-25 12:10:53 -0500

    betty syntax fixes

--- sockets/0-server.c
+++ sockets/0-server.c
@@ -12,39 +12,39 @@ int main(void)
 	int server_fd;
 	struct sockaddr_in address;
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1)
-    {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd == -1)
+	{
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
 
-    int opt = 1;
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+	int opt = 1;
+	setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-    memset(&address, 0, sizeof(address));
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+	memset(&address, 0, sizeof(address));
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(PORT);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
-    {
-        perror("bind failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
+	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+	{
+		perror("bind failed");
+		close(server_fd);
+		exit(EXIT_FAILURE);
+	}
 
-    if (listen(server_fd, 5) < 0)
-    {
-        perror("listen failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
-    printf("Server listening on port %d\n", PORT);
+	if (listen(server_fd, 5) < 0)
+	{
+		perror("listen failed");
+		close(server_fd);
+		exit(EXIT_FAILURE);
+	}
+	printf("Server listening on port %d\n", PORT);
 
-    while (1)
-        pause();
+	while (1)
+		pause();
 
-    close(server_fd);
-    return 0;
+	close(server_fd);
+	return (0);
 }
